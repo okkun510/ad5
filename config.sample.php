96d43267f5f1f8acb4e6c4c519855cd4254a857d
okkun510 <light.life510@hotmail.com>
2021-02-08 16:19:17 +0900

    configを追加

---
+++ config.sample.php
@@ -0,0 +1,5 @@
+<?php
+define('DB_HOST', 'xxx');
+define('DB_NAME', 'xxx');
+define('DB_USER', 'xxxx');
+define('DB_PASS', 'xxxxx');
--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,7 @@
 <?php
+    require('config.php');
     try {
-        $database = new PDO ('mysql:dbname=ad5;host=mysql;charset=utf8mb4', 'root', 'password');
+        $database = new PDO ('mysql:dbname=' . DB_NAME . ';host=' . DB_HOST . ';charset=utf8mb4', DB_USER, DB_PASS);
     } catch(PDOException $e) {
         echo 'DB接続に失敗しました。' . $e->getMessage();
     }
